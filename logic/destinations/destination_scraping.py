0a1b4ecdb66e779f7063b1023e123a8c72063650
Paweł Cichowski <pcichowski@pcichowski.com>
2024-03-05 13:57:15 +0100

    RSWW-18 logic: initialize_scraper: allow creating different webdrivers based on arg
    Add argparse to destination scraper to select platform freely
    
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- logic/destinations/destination_scraping.py
+++ logic/destinations/destination_scraping.py
@@ -1,6 +1,8 @@
+import argparse
 import pathlib
 from typing import Dict, List, AnyStr
 
+from selenium import webdriver
 from tqdm.auto import tqdm
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -73,7 +75,11 @@ def save_dataframe(destinations: Dict[AnyStr, List[AnyStr]]) -> None:
 
 
 if __name__ == "__main__":
-    driver = init_webdriver()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--platform', help='Platform to use for the webdriver, either chrome or firefox')
+    args = parser.parse_args()
+
+    driver = init_webdriver(None if args.platform is None else args.platform.lower())
 
     save_dataframe(scrape_destinations())
 
