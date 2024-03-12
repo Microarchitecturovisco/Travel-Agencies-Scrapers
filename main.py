dc772badfb421464cf148dc7af279d83dacf982d
Paweł Cichowski <pcichowski@pcichowski.com>
2024-03-12 15:08:51 +0100

    RSWW-19 main: collect all tours to results list
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from logic.tours.scrapping_single_tour import scrape_single_tour
 if __name__ == "__main__":
     driver = init_webdriver()
 
-    tours_list = scrape_all_inclusive_tours(driver)
+    tours_urls = scrape_all_inclusive_tours(driver)
 
-    for tour in tours_list:
-        scrape_single_tour(driver, tour)
+    tours_result = []
+
+    for tour in tours_urls[:2]:  # TODO WIP, handle more urls
+        tours_result.append(scrape_single_tour(driver, tour))
 
     # Close the browser
     driver.quit()
